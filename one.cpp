5d3a9490a47d209b04ff9d00144e861b60cd7f94
charudubey20 <61387192+charudubey20@users.noreply.github.com>
2020-04-17 01:12:40 +0530

    simple

---
+++ one.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"enter two numbers"<<endl;
+  cin>>a>>b;
+  c=a+b;
+  cout<<"sum of two numbers"<<endl<<c<<endl;
+  return 0;
+}
+
